253d0acc158c93ccbac72b003cb11a9e87031ec9
BillingtonDev <119828882+BillingtonDev@users.noreply.github.com>
2025-08-04 12:48:55 -0400

    MB - portfolio

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Here's What I Have Been Working On!
+      </h1>
+      <p className="mb-4">
+        {`I am a data analyst studying computer science at Wilfrid Laurier University mastering Python, 
+        SQL, R, and Power BI. As I try to better understand the world around us through data, engineering, 
+        and statistics I also love to get pulled into the worlds of history and economics. While I am 
+        learning how to apply my skills to the insurance industry, I would love to have a chat about your 
+        journey, and how my skills could impact your business make more efficient processes, and better 
+        decision making.`}
+      </p>
+    
+    </section>
+  )
+}
