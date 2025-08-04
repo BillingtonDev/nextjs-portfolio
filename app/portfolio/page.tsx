57c0b1d9ce7265ba6c84316808c150a3c395bdf6
BillingtonDev <119828882+BillingtonDev@users.noreply.github.com>
2025-08-04 13:50:38 -0400

    MB - missing arrow

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -28,59 +28,54 @@ export default function Page() {
         <h2 className="mb-2 text-xl font-semibold tracking-tighter">
             This Website
         </h2 >
-        
-            <a 
-                className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://github.com/BillingtonDev/nextjs-portfolio">
-                <ArrowIcon />
-                <p className="ml-2 h-7">nextjs-portfolio</p>
+        <a 
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/BillingtonDev/nextjs-portfolio">
+            <ArrowIcon />
+            <p className="ml-2 h-7">nextjs-portfolio</p>
             </a>
         
 
         <h2 className="mb-2 text-xl font-semibold tracking-tighter">
             Exploring Image Modeling for Melanoma Detection
         </h2>
-        
-            <a 
-                className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://github.com/BillingtonDev/AIProject">
-                    <ArrowIcon />
-                    <p className="ml-2 h-7">AIProject</p>
-            </a>
+        <a 
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/BillingtonDev/AIProject">
+                <ArrowIcon />
+                <p className="ml-2 h-7">AIProject</p>
+        </a>
         
 
         <h2 className="mb-2 text-xl font-semibold tracking-tighter">
             YouTube Comments Analysis
         </h2>
-        
-            <a 
-                className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://github.com/RupeshRang95/Sentiment_Analysis_Youtube">
-                    <p className="ml-2 h-7">Sentiment_Analysis_Youtube</p>
-            </a>
+        <a 
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/RupeshRang95/Sentiment_Analysis_Youtube">
+                <ArrowIcon />
+                <p className="ml-2 h-7">Sentiment_Analysis_Youtube</p>
+        </a>
         
 
         <h2 className="mb-2 text-xl font-semibold tracking-tighter">
             Basic Discord Bot
         </h2>
-        
-            <a 
-                className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://github.com/BillingtonDev/DiscordBot">
-                    <ArrowIcon />
-                    <p className="ml-2 h-7">DiscordBot</p>
-            </a>
-        
+        <a 
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://github.com/BillingtonDev/DiscordBot">
+                <ArrowIcon />
+                <p className="ml-2 h-7">DiscordBot</p>
+        </a>
       </div>
-
     
     </section>
   )
